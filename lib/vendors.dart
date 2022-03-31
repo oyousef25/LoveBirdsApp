ddcdd49512c28c997fe87c5e2ce0d52e385ceec2
farzana-moury <w0737626@myscc.ca>
2022-03-30 20:32:09 -0400

    #60 Added Another Dialog Box
    Added dialog box for removing saved vendors

--- lib/vendors.dart
+++ lib/vendors.dart
@@ -559,7 +559,88 @@ class _VendorDetailScreenState extends State<VendorDetailScreen> {
                       onPressed: () {
                         if (widget.vendorInfo.saved) {
                           // Case where vendor is already saved
-                          // TODO: Remove a saved vendor
+                          // TODO: Delete a vendor
+                          showDialog<String>(
+                              context: context,
+                              builder: (BuildContext context) => AlertDialog(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0)),
+                            titlePadding: EdgeInsets.zero,
+                            title: Card(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(10),
+                                        topRight: Radius.circular(10),
+                                        bottomLeft: Radius.zero,
+                                        bottomRight: Radius.zero)),
+                                margin: EdgeInsets.zero,
+                                color: Constants.lightSecondary,
+                                child: ListTile(
+                                  title: Text('Deletion Confirmation',
+                                      textAlign: TextAlign.center,
+                                      style: Constants.cardHeaderStyle),
+                                )),
+                            contentPadding: EdgeInsets.only(
+                                top: 20.0,
+                                bottom: 0.0,
+                                left: 25.0,
+                                right: 25.0),
+                            content: Text(
+                                'Are you sure you want to remove this vendor?',
+                                style: Constants.dialogContentStyle),
+                            actionsAlignment: MainAxisAlignment.center,
+                            // actionsPadding: ,
+                            buttonPadding:
+                            EdgeInsets.symmetric(horizontal: 25.0),
+                            actions: <Widget>[
+                              ElevatedButton(
+                                  onPressed: () =>
+                                      Navigator.pop(context, 'Cancel'),
+                                  child: const Text('Cancel',
+                                      style: Constants.buttonRedStyle),
+                                  style: ButtonStyle(
+                                    shape: MaterialStateProperty.all<
+                                        OutlinedBorder>(
+                                      RoundedRectangleBorder(
+                                          borderRadius:
+                                          BorderRadius.circular(10.0)),
+                                    ),
+                                    backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        Constants.buttonRed),
+                                    padding:
+                                    MaterialStateProperty.all<EdgeInsets>(
+                                        EdgeInsets.symmetric(
+                                            vertical: 25.0,
+                                            horizontal: 40.0)),
+                                  )),
+                              ElevatedButton(
+                                onPressed: () => {
+                                  // TODO: Delete guest functionality
+                                  Navigator.pop(context),
+                                  Navigator.pop(context)
+                                },
+                                child: const Text('Remove',
+                                    style: Constants.buttonRedStyle),
+                                style: ButtonStyle(
+                                  shape:
+                                  MaterialStateProperty.all<OutlinedBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                  ),
+                                  backgroundColor:
+                                  MaterialStateProperty.all<Color>(
+                                      Constants.buttonGreen),
+                                  padding:
+                                  MaterialStateProperty.all<EdgeInsets>(
+                                      EdgeInsets.symmetric(
+                                          vertical: 25.0,
+                                          horizontal: 35.0)),
+                                ),
+                              ),
+                            ],
+                          ));
                         } else {
                           // Case where vendor is not saved yet
                           // TODO: Save a vendor
