74e07da60d2bcf625359c4318983bcb4c877a5ef
Hung-Yu Tsai <w0748194@myscc.ca>
2022-04-04 02:47:30 -0400

    #24 Display custom vendors from API request *Added code to retrieve custom vendors from the API and display it as a list in the custom vendors page

--- lib/Vendor/customVendor.dart
+++ lib/Vendor/customVendor.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:lovebirds_app/helper/constants.dart';
+import 'package:http/http.dart' as http;
 
 import '../helper/customVendorInfo.dart';
 import 'customVendorDetail.dart';
@@ -25,39 +28,72 @@ class _CustomVendorScreenState extends State<CustomVendorScreen> {
   // Declare a field that holds the custom vendors list.
   final List<CustomVendorInfo> customVendors;
 
+  /// Gets custom vendor list from API
+  getCustomVendors() async {
+    // Request the custom vendor data, convert to JSon
+    var request = await http.get(Uri.https('oyousef.scweb.ca', '/lovebirds/api/v1/custom-vendors'));
+    var jsonData = jsonDecode(request.body);
+    List<CustomVendorInfo> customVendorsList = [];
+
+    // With the json data, convert it to a CustomVendorInfo and add it to our custom vendors list
+    for(var vendor in jsonData) {
+      CustomVendorInfo customVendor = CustomVendorInfo(vendor['vendor_name'], vendor['vendor_description'], vendor['phone_number'], 'Placeholder', vendor['job_title']);
+      customVendorsList.add(customVendor);
+    }
+
+    // Return the list of custom vendors
+    return customVendorsList;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        // ListView that is built as it is scrolled onto the screen
-        padding: EdgeInsets.only(top: 10.0),
-        itemCount: widget.customVendors.length,
-        itemBuilder: (context, index) {
-          return Card(
-            margin: const EdgeInsets.symmetric(vertical: 7.0, horizontal: 10.0),
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0)),
-            color: Colors.white,
-            shadowColor: Colors.grey,
-            elevation: 5.0,
-            child: ListTile(
-              title: Text(widget.customVendors[index].name,
-                  textAlign: TextAlign.left, style: Constants.listTitleStyle),
-              subtitle: Text(widget.customVendors[index].location,
-                  textAlign: TextAlign.left,
-                  style: Constants.listSubtitleStyle),
-              trailing: Icon(Icons.phone_rounded, size: 40),
-              onTap: () {
-                // Open up the custom vendor info route
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => CustomVendorDetailScreen(
-                      customVendor: widget.customVendors[index]),
-                ));
+      body: FutureBuilder(
+        future: getCustomVendors(),
+        // Takes the snapshotted data
+        builder: (context, AsyncSnapshot snapshot) {
+          // If the data retrieval went wrong
+          if(snapshot.hasError) {
+            return Center(child: Text('There was an error fetching the custom vendor data'),);
+          } else if (snapshot.hasData) { // Successful data retrieval
+            return ListView.builder(
+              // ListView that is built as it is scrolled onto the screen
+              padding: EdgeInsets.only(top: 10.0),
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  margin: const EdgeInsets.symmetric(vertical: 7.0, horizontal: 10.0),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  color: Colors.white,
+                  shadowColor: Colors.grey,
+                  elevation: 5.0,
+                  child: ListTile(
+                    title: Text(snapshot.data[index].name,
+                        textAlign: TextAlign.left, style: Constants.listTitleStyle),
+                    subtitle: Text(snapshot.data[index].location,
+                        textAlign: TextAlign.left,
+                        style: Constants.listSubtitleStyle),
+                    trailing: Icon(Icons.phone_rounded, size: 40),
+                    onTap: () {
+                      // Open up the custom vendor info route
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => CustomVendorDetailScreen(
+                            customVendor: snapshot.data[index]),
+                      ));
+                    },
+                  ),
+                );
               },
-            ),
-          );
-        },
-      ),
+            );
+          } else {
+            // Loading data animation
+            return const Center(
+                child: CircularProgressIndicator(),
+            );
+          }
+        }
+      )
     );
   }
 }
--- lib/guests.dart
+++ lib/guests.dart
@@ -38,7 +38,78 @@ class _GuestsPageState extends State<GuestsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
+      body: NestedScrollView(
+        // Setting floatHeaderSlivers to true is required in order to float
+        // the outer slivers over the inner scrollable.
+        floatHeaderSlivers: true,
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              floating: true,
+              expandedHeight: 203.0,
+              collapsedHeight: 203.0,
+              backgroundColor: Colors.white,
+              forceElevated: innerBoxIsScrolled,
+              flexibleSpace: Stack(
+                // Image of the vendor (can be cached)
+                children: [
+                  Column(
+                    children: [
+                      Card(
+                          margin: const EdgeInsets.symmetric(
+                              vertical: 25.0,
+                              horizontal: 100.0), // Hack for shrinking card padding
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0)),
+                          color: Constants.lightSecondary,
+                          child: const ListTile(
+                              minVerticalPadding: 30.0,
+                              title: Text('Total Guests',
+                                  textAlign: TextAlign.center,
+                                  style: Constants.cardHeaderStyle),
+                              subtitle: Text('XX Guests',
+                                  textAlign: TextAlign.center,
+                                  style: Constants.cardContentStyle))),
+                      Wrap(
+                          spacing: 30.0,
+                          // Creates a list of 3 chips that will highlight the selected ones
+                          children: List<Widget>.generate(
+                            3,
+                                (int index) {
+                              return ChoiceChip(
+                                label: Text(_chips[index]),
+                                labelStyle: _selectedIndex == index
+                                    ? Constants.chipSelectedStyle
+                                    : Constants.chipUnselectedStyle,
+                                selected: _selectedIndex ==
+                                    index, // If selected index IS the index then it is selected
+                                backgroundColor: Colors.white,
+                                shadowColor: Colors.grey,
+                                elevation: 2.0,
+                                selectedColor: Constants.darkSecondary,
+                                onSelected: (bool selected) {
+                                  // Toggles selected chip only if the user presses on an unselected chip
+                                  if (_selectedIndex != index) {
+                                    setState(() {
+                                      _selectedIndex = selected ? index : null;
+                                    });
+                                  }
+                                },
+                              );
+                            },
+                          ).toList()),
+                      // Some padding before the guest list
+                      Padding(
+                          padding:
+                          EdgeInsets.all(5.0)),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ];
+        },
+        body: SingleChildScrollView(
           scrollDirection: Axis.vertical,
           child: SizedBox(
             height: 800,
@@ -46,52 +117,6 @@ class _GuestsPageState extends State<GuestsPage> {
               // Column containing 4 rows:
               // 1. Total guests card, 2. Choice chips for filtering, 3. Padding, 4. Guest list
               children: <Widget>[
-                Card(
-                    margin: const EdgeInsets.symmetric(
-                        vertical: 25.0,
-                        horizontal: 100.0), // Hack for shrinking card padding
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0)),
-                    color: Constants.lightSecondary,
-                    child: const ListTile(
-                        minVerticalPadding: 30.0,
-                        title: Text('Total Guests',
-                            textAlign: TextAlign.center,
-                            style: Constants.cardHeaderStyle),
-                        subtitle: Text('XX Guests',
-                            textAlign: TextAlign.center,
-                            style: Constants.cardContentStyle))),
-                Wrap(
-                    spacing: 30.0,
-                    // Creates a list of 3 chips that will highlight the selected ones
-                    children: List<Widget>.generate(
-                      3,
-                          (int index) {
-                        return ChoiceChip(
-                          label: Text(_chips[index]),
-                          labelStyle: _selectedIndex == index
-                              ? Constants.chipSelectedStyle
-                              : Constants.chipUnselectedStyle,
-                          selected: _selectedIndex ==
-                              index, // If selected index IS the index then it is selected
-                          backgroundColor: Colors.white,
-                          shadowColor: Colors.grey,
-                          elevation: 2.0,
-                          selectedColor: Constants.darkSecondary,
-                          onSelected: (bool selected) {
-                            // Toggles selected chip only if the user presses on an unselected chip
-                            if (_selectedIndex != index) {
-                              setState(() {
-                                _selectedIndex = selected ? index : null;
-                              });
-                            }
-                          },
-                        );
-                      },
-                    ).toList()),
-                Padding(
-                    padding:
-                    EdgeInsets.all(5.0)), // Some padding before the guest list
                 Expanded(
                   // Makes sure that ListView expands to fit inside the column
                   child: ListView.builder(
@@ -132,6 +157,7 @@ class _GuestsPageState extends State<GuestsPage> {
                 )
               ],
             ),),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
